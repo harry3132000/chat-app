3ab461a138fe89ec87e9dc7ebcb2ffce23779b94
harry3132000 <86356531+harry3132000@users.noreply.github.com>
2021-06-23 17:22:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require('http').createServer(app);
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+//USING EXPRESS MIDDELWARE
+app.use(express.static(__dirname + '/public'));
+
+//create routing
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname + '/index.html');
+} )
+
+
+//SOCKET
+
+const io = require('socket.io')(http) 
+
+io.on("connection", (socket) =>{
+    console.log("Connected...")
+    socket.on('message', (msg) =>{
+        socket.broadcast.emit('message', msg)
+    })
+})
